206ff5e51e6e9102be190038230cddab977df58a
Anirudh <anirudhbs96@gmail.com>
2017-12-23 16:02:22 +0530

    Add moveToLocation function for map

--- public/script/map.js
+++ public/script/map.js
@@ -4,12 +4,15 @@ socket.on('latestCords', coords => {
   console.log('coords', coords)
 })
 
+let map
 window.onload = () => {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      const map = drawMap(position.coords.latitude, position.coords.longitude)
-      pointOnMap(map, position.coords.latitude, position.coords.longitude)
-      drawLine(map, {lat: 20, lng: 24}, {lat: 1, lng: 20})
+      drawMap(position.coords.latitude, position.coords.longitude)
+      pointOnMap(position.coords.latitude, position.coords.longitude)
+      pointOnMap(position.coords.latitude + 10, position.coords.longitude + 10)      
+      drawLine({lat: 20, lng: 24}, {lat: 1, lng: 20})
+      setTimeout(function(){ moveToLocation(30, 40) }, 5000)
     })
   } else {
     // Location not available
@@ -18,14 +21,14 @@ window.onload = () => {
 
 function drawMap(lat, lng) {
   const coordinates = { lat, lng }
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
     center: coordinates
   })
-  return map
+  // return map
 }
 
-function pointOnMap(map, lat, lng) {
+function pointOnMap(lat, lng) {
   const coordinates = { lat, lng }
   const marker = new google.maps.Marker({
     position: coordinates,
@@ -33,7 +36,7 @@ function pointOnMap(map, lat, lng) {
   })
 }
 
-function drawLine(map, source, destination) {
+function drawLine(source, destination) {
   const line = new google.maps.Polyline({
     path: [
       new google.maps.LatLng(source),
@@ -44,4 +47,10 @@ function drawLine(map, source, destination) {
     strokeWeight: 2,
     map: map
   })
+}
+
+function moveToLocation(lat, lng){
+  const center = new google.maps.LatLng(lat, lng)
+  map.panTo(center)
+  map.setZoom(12)
 }
