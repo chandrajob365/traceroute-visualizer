aa837be9cd297eede0771b96db8e3b9c8837bfce
Manish <chandrajob365@gmail.com>
2018-01-02 00:58:18 +0530

    Updated relevant callback handlers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-const TraceRoute = require('./lib/traceroute_bkp')
+const ping = require('./lib/traceroute')
 app.use(express.static('public'))
 
 server.listen(process.env.PORT || 8000, () => {
@@ -10,13 +10,19 @@ server.listen(process.env.PORT || 8000, () => {
 })
 
 io.sockets.on('connection', socket => {
-  socket.on('destination', (data) => {
-    new TraceRoute().trace(data, (err, data) => {
-      if (err) socket.emit('errorMsg', err)
-      else {
-        data.status === 'inprogress'
-          ? socket.emit('coords', data)
-          : socket.emit('completed')
+  socket.on('destination', target => {
+    ping.createSession().traceRoute(target, (err, data) => {
+      if (err) {
+        if (err.name === 'DNSError') socket.emit('errorMsg', err.message)
+      } else {
+        console.log('[Inside app] data.latitude = ', data.latitude, ' data.longitude = ', data.longitude, ' data.source = ', data.source, ' data.target = ', data.target, ' status = ', data.status)
+        socket.emit('coords', {
+          source: data.source,
+          lat: data.latitude,
+          lng: data.longitude,
+          status: data.status,
+          target: target
+        })
       }
     })
   })
